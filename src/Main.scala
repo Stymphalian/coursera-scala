5007503c75ae389d33a0cff3cfdbe11523670123
Stymphalian <saturnslight@gmail.com>
2015-04-12 03:08:46 -0700

    Lecture 1.6

--- src/Main.scala
+++ src/Main.scala
@@ -2,19 +2,19 @@ object session{
     def abs(x :Double) = if( x < 0) -x else x
 
     def sqrt(x:Double) = {
-        def sqrtIter(guess:Double, x:Double): Double =
-        if(isGoodEnough(guess,x))
+        def sqrtIter(guess:Double): Double =
+        if(isGoodEnough(guess))
             guess
         else
-            sqrtIter(improve(guess,x),x)
+            sqrtIter(improve(guess))
 
-        def isGoodEnough(guess:Double, x:Double) =
-            abs(guess*guess - x ) < 0.001
+        def isGoodEnough(guess:Double) =
+            abs(guess*guess - x)/x < 0.001
 
-        def improve(guess:Double,x:Double) =
+        def improve(guess:Double) =
             (guess + x/guess)/2
 
-        sqrtIter(1.0,x)
+        sqrtIter(1.0)
     }
 
 }
