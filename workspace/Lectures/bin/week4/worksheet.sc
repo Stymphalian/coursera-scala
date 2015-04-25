a3db781d90fe792642bd485f9f13de7209c62890
Stymphalian <saturnslight@gmail.com>
2015-04-24 18:01:23 -0700

    Lecture 4.7 : Lists

--- workspace/Lectures/bin/week4/worksheet.sc
+++ workspace/Lectures/bin/week4/worksheet.sc
@@ -1,5 +1,4 @@
 import week4._
 object Scratch {
-  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
-  val x = 5                                       //> x  : Int = 5
+ 
 }
--- workspace/Lectures/src/week4/Lecture4.7.scala
+++ workspace/Lectures/src/week4/Lecture4.7.scala
@@ -1,5 +1,30 @@
 package week4
 
 object lecture47 {
+  
+  val fruit = List("apples","oranges","pears")
+  val nums = List(1,2,3,4)
+  // lists are all homongenous
+  var diag3 = List(List(1,0,0),List(0,1,0),List(0,0,1))
+  
+  //fruit = "apples" :: ("oranges" :: ("pears" :: Nil))  
+//  1 :: 2 :: xs
+//  x :: Nil
+//  List(x)
+//  List()
+//  List( 2 :: xs)
+  
+  def insert(x:Int, xs:List[Int]):List[Int] = xs match {
+    case List() =>  List(x) 
+    case y :: ys => {
+      if( y > x)  x :: y :: ys      
+      else y :: insert(x,ys)
+    }
+  }
+  
+  def isort( xs:List[Int]): List[Int] = xs match{
+    case List() => List()
+    case y :: ys => insert(y, isort(ys))
+  }
 
 }
